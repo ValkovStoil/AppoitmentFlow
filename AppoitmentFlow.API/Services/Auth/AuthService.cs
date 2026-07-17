9f1beb001634dad0a5f6add54d54a77d0a604938
ValkovStoil <valkovstoil@abv.bg>
2026-07-17 20:12:25 +0300

    implementing login flow and starting Jwt authentication

--- AppoitmentFlow.API/Services/Auth/AuthService.cs
+++ AppoitmentFlow.API/Services/Auth/AuthService.cs
@@ -4,20 +4,29 @@ using AppoitmentFlow.API.DTOs.Auth;
 using AppoitmentFlow.API.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AppoitmentFlow.API.Services.Auth
 {
-    public class AuthService : IAuthService
+    public class AuthService : IAuthService, IConfiguration
     {
         private readonly AppDbContext _context;
         private readonly PasswordHasher<User> _passwordHasher;
+        private readonly IConfiguration _configuration;
 
-        public AuthService(AppDbContext context)
+        public string? this[string key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public AuthService(AppDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
             _passwordHasher = new PasswordHasher<User>();
         }
-
+       
 
         public async Task<RegisterResponseDTO> RegisterAsync(RegisterRequestDTO request)
         {
@@ -72,12 +81,51 @@ namespace AppoitmentFlow.API.Services.Auth
                 throw new Exception(CustomMasseges.InvalidEmailOrPasword);
             }
 
+            var token = GenerateJwtToken(user);
+
             return new LoginResponseDTO
             {
                 Id = user.Id,
                 Email = user.Email,
-                Token = string.Empty
+                Token = token
             };
+        }
+
+        public IConfigurationSection GetSection(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IConfigurationSection> GetChildren()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChangeToken GetReloadToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Isssuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires:DateTime.UtcNow.AddDays(7),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
